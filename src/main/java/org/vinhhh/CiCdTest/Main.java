9739bf4811ca18ee37b00311caf391431ea0cadf
VinhHoHuu <thuydungpham8419@gmail.com>
2026-05-29 13:16:57 +0700

    Add CD build jar configuration

--- src/main/java/org/vinhhh/CiCdTest/Main.java
+++ src/main/java/org/vinhhh/CiCdTest/Main.java
@@ -4,6 +4,12 @@ package org.vinhhh.CiCdTest;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        DayTimeChecker checker = new DayTimeChecker();
 
+        System.out.println("DayTimeChecker Demo");
+        System.out.println("25/05/2026: " + checker.isValidDate(25, 5, 2026));
+        System.out.println("32/01/2026: " + checker.isValidDate(32, 1, 2026));
+        System.out.println("29/02/2024: " + checker.isValidDate(29, 2, 2024));
+        System.out.println("29/02/2025: " + checker.isValidDate(29, 2, 2025));
     }
 }
